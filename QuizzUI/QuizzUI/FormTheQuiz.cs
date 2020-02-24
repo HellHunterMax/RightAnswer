b2529368da01c2ef2cb70165f8926d9633d526bc
Max <hellhunter15@hotmail.com>
2020-02-24 06:03:46 +0100

    changed some naming and placing for readability.

--- QuizzUI/QuizzUI/FormTheQuiz.cs
+++ QuizzUI/QuizzUI/FormTheQuiz.cs
@@ -45,7 +45,7 @@ namespace QuizzUI
 
 
         /// <summary>
-        /// Creates a panel with all the possible answers for the asked question.
+        /// Creates a panel for the question with all the possible answers for the asked question.
         /// </summary>
         /// <param name="theQuestion"></param>
         private void CreatePanelForQuestion(QuestionModel theQuestion)
@@ -62,57 +62,14 @@ namespace QuizzUI
             MyExtentions.Shuffle<string>(questionList); // shuffle the list.
             int numberOfAnswers = questionList.Count;
 
-            Label LabelQuestionNumbering = new Label
-            {
-                AutoSize = true,
-                //Font = new Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0))),
-                ForeColor = Color.Green,
-                Location = new Point(284, 26),
-                Name = "LabelQuestionNumbering",
-                Size = new Size(415, 45),
-                TabIndex = 0,
-                Text = "Question Number " + (numberOfQuestionsAsked + 1).ToString() + "/" + numberOfQuestionsToBeAsked.ToString()
-            };
-            Label LabelQuestion = new Label
-            {
-                AutoSize = true,
-                ForeColor = Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0))))),
-                Location = new Point(12, 71),
-                MaximumSize = new Size(1000, 0),
-                Name = "LabelQuestion",
-                Size = new Size(991, 128),
-                TabIndex = 1,
-                Text = theQuestion.Question
-            };
-            Label LabelScore = new Label
-            {
-                AutoSize = true,
-                //Font = new Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0))),
-                ForeColor = Color.Green,
-                Location = new Point(600, 26),
-                Name = "LabelScore",
-                Size = new Size(415, 45),
-                TabIndex = 2,
-                Text = "Score: " + score.ToString() + " Streak: " + streak.ToString()
-            };
-
-
-            //Creating a panel with answers.
+            //Creating a panel for the question with answers for easy dispose.
             Panel Panel1 = new Panel
             {
                 Size = new Size(1000, (numberOfAnswers * 75) + 200),
                 Location = new Point(12, 12)
             };
-            for (int tileNumber = 0; tileNumber < numberOfAnswers; tileNumber++)
-            {
-                QuestionTile tile = new QuestionTile(tileNumber, questionList[tileNumber]);
-                tile.MouseDown += QuestionTile_MouseDown;
-                Panel1.Controls.Add(tile);
-            }
+            AddQuestionLabelsAndAnswerButtons();
 
-            Panel1.Controls.Add(LabelQuestionNumbering);
-            Panel1.Controls.Add(LabelQuestion);
-            Panel1.Controls.Add(LabelScore);
             this.Controls.Add(Panel1);
 
             void QuestionTile_MouseDown(object sender, MouseEventArgs e)
@@ -136,29 +93,70 @@ namespace QuizzUI
                 Panel1.Dispose();
 
                 NextQuestion();
-                //TODO when tile is pressed check if its the right answer >> add score >> next question.
 
+            }
+
+            void AddQuestionLabelsAndAnswerButtons()
+            {
+                Label LabelQuestionNumbering = new Label
+                {
+                    AutoSize = true,
+                    ForeColor = Color.Green,
+                    Location = new Point(284, 26),
+                    Name = "LabelQuestionNumbering",
+                    Size = new Size(415, 45),
+                    TabIndex = 0,
+                    Text = "Question Number " + (numberOfQuestionsAsked + 1).ToString() + "/" + numberOfQuestionsToBeAsked.ToString()
+                };
+                Label LabelQuestion = new Label
+                {
+                    AutoSize = true,
+                    ForeColor = Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0))))),
+                    Location = new Point(12, 71),
+                    MaximumSize = new Size(1000, 0),
+                    Name = "LabelQuestion",
+                    Size = new Size(991, 128),
+                    TabIndex = 1,
+                    Text = theQuestion.Question
+                };
+                Label LabelScore = new Label
+                {
+                    AutoSize = true,
+                    ForeColor = Color.Green,
+                    Location = new Point(600, 26),
+                    Name = "LabelScore",
+                    Size = new Size(415, 45),
+                    TabIndex = 2,
+                    Text = "Score: " + score.ToString() + " Streak: " + streak.ToString()
+                };
+                for (int tileNumber = 0; tileNumber < numberOfAnswers; tileNumber++)
+                {
+                    QuestionTile tile = new QuestionTile(tileNumber, questionList[tileNumber]);
+                    tile.MouseDown += QuestionTile_MouseDown;
+                    Panel1.Controls.Add(tile);
+                }
+
+                Panel1.Controls.Add(LabelQuestionNumbering);
+                Panel1.Controls.Add(LabelQuestion);
+                Panel1.Controls.Add(LabelScore);
             }
         }
 
         
         private class QuestionTile : Button
         {
-            //internal Point GridPosition { get; }
             internal QuestionTile(int tileNumber, string answerText)
             {
                 this.Name = $"Question_{tileNumber}";
                 this.Size = new Size(940, 75);
                 this.Location = new Point(12,(tileNumber * 75) + 200);
                 this.TabIndex = 10 + tileNumber;
-                //this.GridPosition = new Point(12, tileNumber);
                 this.Text = answerText;
             }
-
         }
 
         /// <summary>
-        /// Gets a list of possible Questions.
+        /// Gets a list of possible Questions from the SQL server.
         /// </summary>
         private void GetQuestionsList()
         {
